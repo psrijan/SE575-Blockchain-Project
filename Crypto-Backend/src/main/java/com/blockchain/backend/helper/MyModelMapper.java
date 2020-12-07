de49191a7be5148ed129f69c0d0e872bdec92c45
ameerj <52414509+ameerj@users.noreply.github.com>
2020-12-07 17:22:18 -0500

    remove remaining crypto references

--- Crypto-Backend/src/main/java/com/blockchain/backend/helper/MyModelMapper.java
+++
@@ -1,8 +0,0 @@
-package com.crypto.backend.helper;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MyModelMapper extends ModelMapper {
-}
--- Crypto-Backend/src/main/java/com/blockchain/backend/repository/IBaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crypto.backend.repository;
-
-import com.crypto.backend.entities.BaseMessage;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IBaseRepository extends CrudRepository<BaseMessage, Long> {
-}
